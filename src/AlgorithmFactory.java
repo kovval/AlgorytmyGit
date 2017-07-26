cff960a117c3835f9fd9d3c70c563465716526d3
RENT-Komputer\RENT <jan.kisiel22@gmail.com>
2017-07-26 17:46:07 +0200

    no message

---
+++ src/AlgorithmFactory.java
@@ -0,0 +1,33 @@
+import Algorithms.*;
+
+/**
+ * Created by jake on 21.07.2017.
+ */
+public class AlgorithmFactory {
+    public AbstractAlgorithm getAlgorithm(String name) throws Exception {
+        switch (name.toLowerCase()){
+            case "test":
+                return new Test();
+            case "biednycamelcase":
+                return new BiednyCamelCase();
+            case "sumowanieliczbnaturalnych":
+                return new SumowanieLiczbNaturalnych();
+            case "odejmowaniekwoty":
+                return new OdejmowanieKwoty();
+            case "sumaliczbwejsciowych":
+                return new SumaLiczbWejsciowych();
+            case "rokprzestepny":
+                return new RokPrzestepny();
+            case "silnia":
+                return new Silnia();
+            case "fibonacci":
+                return new Fibonacci();
+            case "szyfrcezara":
+                return new SzyfrCezara();
+            case "wyznaczaniecyfrsilni":
+                return new WyznaczanieCyfrSilni();
+            default:
+                throw new Exception("Nie ma takiego algorytmu!");
+        }
+    }
+}
