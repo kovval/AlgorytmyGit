880f12edab4275f73c0b837a528de4b8806ec339
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-07-30 10:13:43 +0200

    Zadanie z polem jasia

--- src/Algorithms/NowaDzialka.java
+++ src/Algorithms/NowaDzialka.java
@@ -11,6 +11,20 @@ public class NowaDzialka extends AbstractAlgorithm {
 
     @Override
     public String runAlgorithm(String[] input) {
+
+        double[] poleDzialki = new double[input.length];
+        for (int i=1; i< input.length;i++){
+            int dlugoscDzialki = Integer.parseInt(input[i]);
+            poleDzialki[i]=dlugoscDzialki*dlugoscDzialki;
+        }
+
+        for(int j=0; j<input.length-1;j++) {
+            System.out.printf("\nPole dzialki wyznaczone przez jasia to: %2.2f", poleDzialki[j]);// Można by wcisnac zmienna odnosząca się do wieku, wiek odnosiłby się do wzrostu,
+            // a wzrost przybliżał by do pełnego określenia ile taka na prawde zajmuje metrów jeden krok jasia.
+            // no ale, tak zadanie można wyznaczyć szybciej... zajme siętym jak skończe inne zadanka
+        }
+
+
         return null;
     }
 }
