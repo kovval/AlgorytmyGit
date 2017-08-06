cc373e6e2d7ce27be12a7559e5e3bdecbf293322
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-08-06 20:12:44 +0200

    poprawki

--- src/Struktury/OneWayList.java
+++ src/Struktury/OneWayList.java
@@ -8,6 +8,12 @@ public class mOneWayList {
     Element _iterator;
     int counter;
 
+    public void addElementAtBeginning(int newElement) {
+        OneWayListElement p = new OneWayListElement();
+        p.data = newElement;
+    }
+
+
     public void addAtLast (int newElement, mOneWayList list) {
       Element p = new Element();
         p.data = newElement;
@@ -20,6 +26,6 @@ public class mOneWayList {
 
     }
 
-    public void
+//    public void
 
 }
