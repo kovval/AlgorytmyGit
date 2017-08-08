f74e9833813e8068dcc1a03a34d859e81293cfa4
RENT-Komputer\RENT <jan.kisiel22@gmail.com>
2017-08-08 20:54:56 +0200

    no message

--- src/Algorithms/OdwrotnaNotacjaPolskaZad.java
+++ src/Algorithms/OdwrotnaNotacjaPolskaZad.java
@@ -5,18 +5,18 @@ import java.util.Stack;
 public class OdwrotnaNotacjaPolskaZad extends AbstractAlgorithm {
     @Override
     public String getName() {
-        return null;
+        return "ONTzAd";
     }
 
     @Override
-    public void runAlgorithm(String[] input) {
-        String enter = "(1 + 2 * 3 - 4) / 6 ^ 7 + 4) / 3 = ";
-        String[] inputArray = enter.split(" ");
+    public void runAlgorithm(String[] input){
+        String myInput = " a + b  * c =";
+        String[] myInputArr = myInput.split(" ");
 
         Stack<String> stack = new Stack<>();
         String helper = "";
 
-        for (String element : inputArray) {
+        for (String element : myInputArr) {
             switch (element) {
                 case "=":
                     while (!stack.empty()) {
@@ -25,6 +25,7 @@ public class OdwrotnaNotacjaPolskaZad extends AbstractAlgorithm {
                     break;
                 case "(":
                     stack.push(element);
+                    break;
                 case ")":
                     helper = stack.pop();
                     while (!helper.equals("(")) {
@@ -38,22 +39,23 @@ public class OdwrotnaNotacjaPolskaZad extends AbstractAlgorithm {
                 case "/":
                 case "^":
                     while (!stack.empty()) {
-                        if ((GetOperatorLevel(element) == 3) || GetOperatorLevel(element) >= GetOperatorLevel(stack.peek())) {
+                        if ((GetOperatorPriority(element) == 3) || (GetOperatorPriority(element) >= GetOperatorPriority(stack.peek()))) {
                             break;
-                        } else
+                        } else {
                             System.out.printf("%s", stack.pop());
+                        }
                     }
                     stack.push(element);
                     break;
                 default:
                     System.out.printf("%s", element);
             }
-
         }
     }
 
-    private int GetOperatorLevel(String element) {
-        switch (element) {
+
+    public int GetOperatorPriority(String operator) {
+        switch (operator) {
             case "+":
             case "-":
                 return 1;
@@ -65,8 +67,63 @@ public class OdwrotnaNotacjaPolskaZad extends AbstractAlgorithm {
             default:
                 return -1;
         }
-
     }
 }
 
 
+//        String enter = "(1 + 2 * 3 - 4) / 6 ^ 7 + 4) / 3 = ";
+//        String[] inputArray = enter.split(" ");
+//
+//        Stack<String> stack = new Stack<>();
+//        String helper = "";
+//
+//        for (String element : inputArray) {
+//            switch (element) {
+//                case "=":
+//                    while (!stack.empty()) {
+//                        System.out.printf("%s", stack.pop());
+//                    }
+//                    break;
+//                case "(":
+//                    stack.push(element);
+//                case ")":
+//                    helper = stack.pop();
+//                    while (!helper.equals("(")) {
+//                        System.out.printf("%s", helper);
+//                        helper = stack.pop();
+//                    }
+//                    break;
+//                case "+":
+//                case "-":
+//                case "*":
+//                case "/":
+//                case "^":
+//                    while (!stack.empty()) {
+//                        if ((GetOperatorLevel(element) == 3) || GetOperatorLevel(element) >= GetOperatorLevel(stack.peek())) {
+//                            break;
+//                        } else
+//                            System.out.printf("%s", stack.pop());
+//                    }
+//                    stack.push(element);
+//                    break;
+//                default:
+//                    System.out.printf("%s", element);
+//            }
+//
+//        }
+//    }
+//
+//    private int GetOperatorLevel(String element) {
+//        switch (element) {
+//            case "+":
+//            case "-":
+//                return 1;
+//            case "*":
+//            case "/":
+//                return 2;
+//            case "^":
+//                return 3;
+//            default:
+//                return -1;
+//        }
+//
