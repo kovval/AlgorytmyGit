5b97df0d04df28d87108cc97f819851f43fb6dae
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-08-12 13:29:11 +0200

    kolejne zadanko pyklo

--- src/Algorithms/IDNumberWeryfikacja.java
+++ src/Algorithms/IDNumberWeryfikacja.java
@@ -2,6 +2,8 @@ package Algorithms;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.text.CharacterIterator;
+
 public class IDNumberWeryfikacja extends AbstractAlgorithm {
     @Override
     public String getName() {
@@ -16,26 +18,28 @@ public class IDNumberWeryfikacja extends AbstractAlgorithm {
     public void runAlgorithm(String[] input) {
         int [] weigth = {7, 3, 1, 9, 7, 3, 1, 7, 3}; // tablica wag
         int [] veryficator = new int[9];
-        int ten = 10;
         String iD = input[1];
         char[] iDNumber = iD.toCharArray();
+        int test = 0;
 
-        for (int iDCharsCounter = 0; iDCharsCounter <= 2; iDCharsCounter++) {
+        for (int iDCharsCounter = 0; iDCharsCounter < 9; iDCharsCounter++) {
             for(int i = 0; _letters.length > i; i++) {
-
-            if(_letters[i] == (iDNumber[iDCharsCounter])) {
-                veryficator[iDCharsCounter] = i;
+            if(_letters[i] == iDNumber[iDCharsCounter] && iDCharsCounter<3) {
+                veryficator[iDCharsCounter] = i+10;
+            } if(iDCharsCounter >= 3){
+                    veryficator[iDCharsCounter] = Character.getNumericValue(iDNumber[iDCharsCounter]);
             }
             }
-            System.out.println(veryficator[0] + veryficator[1] + veryficator[2]);
         }
-
-
-
-
-
-
-
+            for(int i = 0; i < veryficator.length; i++) {
+                veryficator[i] = veryficator[i]*weigth[i];
+                test += veryficator[i];
+            }
+            if(test % 10 == 0) {
+                System.out.print("Weryfikacja numeru dowodu osobistego przebiegła pomyślnie!");
+            } else {
+                System.out.println("Mamy nielegalnego imigranta, wyślijmy go do berlina ");
+            }
 
     }
 }
