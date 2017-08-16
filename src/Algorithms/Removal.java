979fc60d0b1d546d432b62d1c064ead61a6abe09
kovval <jan.kisiel22@gmail.com>
2017-08-16 08:14:19 +0200

    Update Removal.java

--- src/Algorithms/Removal.java
+++ src/Algorithms/Removal.java
@@ -10,30 +10,30 @@ public class Removal extends AbstractAlgorithm {
 
     @Override
     public void runAlgorithm(String[] input) {
-        int amount = input.length-2;
-        int capacitanceLuggage = Integer.parseInt(input[1]);
-        int[] luggage = new int[amount];
-        int counter = 0;
-        for(int i = 0; i<amount;i++) {
-            luggage[i] = Integer.parseInt(input[i+2]);
+        int amount = input.length-2;//podajemyliczbe bagażów na podstawie długości talbicy
+        int capacitanceLuggage = Integer.parseInt(input[1]); // pojemność kartonu
+        int[] luggage = new int[amount]; // tablice pakunków
+        int counter = 0;    // zerujemy liczbe pakunków
+        for(int i = 0; i<amount;i++) { 
+            luggage[i] = Integer.parseInt(input[i+2]); // wypisujemy do tablice wagi bagazy
         }
-            int a = luggage.length;
+            int a = luggage.length; // wychodzi na to, że to jest niepotrzebne, no ale niech zostanie
               for(int i=0; i < a ; i++) {
                   int weighPrim = capacitanceLuggage;
 
-                for(int j =0 ;luggage.length > j ;j++ ) {
+                for(int j =0 ;luggage.length > j ;j++ ) {//lecimy po wszystkich paunktach, aż nie skończymy z załadunkiem
 
-                    if(weighPrim < capacitanceLuggage && weighPrim >= luggage[j]) {
-                        weighPrim = weighPrim-luggage[j];
+                    if(weighPrim < capacitanceLuggage && weighPrim >= luggage[j]) {//ta pętla ma za zadanie wypełniać wolne 
+                        weighPrim = weighPrim-luggage[j];                           //pojemności do ładowania bagazu
                         luggage[j] = 0;
                     }
 
-                    if(weighPrim >= luggage[j] && luggage[j] != 0) {
+                    if(weighPrim >= luggage[j] && luggage[j] != 0) {// w tą pętle ląduje pakunek zawsze jako pierwszy
                         weighPrim = weighPrim-luggage[j];
                         luggage[j] = 0;
                         counter++;
                     }
-                    if(weighPrim == 0) {
+                    if(weighPrim == 0) { // gdybyśmy załadowali bagaż całkowicie
                         break;
                     }
                 }
