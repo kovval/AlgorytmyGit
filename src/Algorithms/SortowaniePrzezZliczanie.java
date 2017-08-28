e19f2006253ec4bcd05fb613a391d9ae8d3e79dd
RENT-Komputer\RENT <jan.kisiel22@gmail.com>
2017-08-28 20:48:09 +0200

    no message

--- src/Algorithms/SortowaniePrzezZliczanie.java
+++ src/Algorithms/SortowaniePrzezZliczanie.java
@@ -1,36 +1,50 @@
 package Algorithms;
 
 /**
- * Created by RENT on 2017-07-27.
+ * Created by jake on 27.07.2017.
  */
 public class SortowaniePrzezZliczanie extends AbstractAlgorithm {
+
     @Override
     public String getName() {
-        return "sortowanie przez zliczanie";
+        return "Sortowanie przez zliczanie";
     }
 
     @Override
-    public void runAlgorithm(String[] input) {
+    public void runAlgorithm(String[] input)
+    {
+        int n = input.length - 2; //pobieramy sobie długość ciągu jaki mamy posortować
+        int max = Integer.parseInt(input[1]) + 1; //jako pierwszy agrument po nazwie algorytmu dany jest element maksymalny
+        int[] tabSparsowanychWejscia = new int[n]; //tworzymy nową tablicę, do której sparsujemy nasz wejściowy ciąg do posortowania
 
-        int n = input.length - 2;
-        int maksimumTablicy = Integer.parseInt(input[1]) + 1;
-        int[] tabSparsowanych = new int[n];
-
-        for (int i = 0; i < tabSparsowanych.length; i++) {
-            tabSparsowanych[i] = Integer.parseInt(input[i + 2]);
+        for (int i = 0; i < n; i++)
+        {
+            tabSparsowanychWejscia[i] = Integer.parseInt(input[i+2]); //parsujemy po kolei wszystkie liczby
+            //pobieramy z input wartości przesunięte o 2, ponieważ
+            //pod input[0] siedzi nazwa alg, input[1] element maksymalny
+            //więc +2 (rozpoczynamy iteracje od 0)
         }
-        int[] tablicaDoZliczania = new int[maksimumTablicy];
-        for (int i = 0; i < tabSparsowanych.length; i++) {
-            int liczba = tabSparsowanych[i];
-            tablicaDoZliczania[liczba]++;
+
+        int[] tablicaDoZliacznia = new int[max]; //tworzymy sobie nową tablicę, która nam posłuży do zliczania
+        //kolejnych elementów
+        for (int i = 0; i < tabSparsowanychWejscia.length; i++ ) //przechodzimy po każdej liczbie którą chcemy zliczyć
+        {
+            int liczba = tabSparsowanychWejscia[i]; //pobieramy sobie tą liczbę
+            tablicaDoZliacznia[liczba]++; //odwołujemy się do jej indeksu tablicy i zwiększamy o 1 wartość, \
+            // która się kryje pod tym indeksem
         }
 
-        for (int indeksTablicy = 0; indeksTablicy < tablicaDoZliczania.length; indeksTablicy++) {
+        for(int indeksTablicy = 0; indeksTablicy < tablicaDoZliacznia.length; indeksTablicy++) //przechodzimy po całej tablicy
+        //służącej nam do zliczania
+        {
+            if(tablicaDoZliacznia[indeksTablicy] > 0) //Rozpatrujemy tylko przypadki, gdzie wystąpiły jakieś liczby
+                for(int ileLiczb = tablicaDoZliacznia[indeksTablicy]; ileLiczb > 0; ileLiczb--)
+                //pobieramy liczbę wystąpień danej liczby, a następnie zmniejszamy ją do 1 aż osiągnie 0 za każdym
+                //razem ją drukując
+                {
 
-            for (int ileLiczb = tablicaDoZliczania[indeksTablicy]; ileLiczb != 0; ileLiczb--) {
-
-                System.out.printf("%d ", indeksTablicy);
-            }
+                    System.out.printf("%d ", indeksTablicy); //drukujemy liczbę
+                }
         }
     }
 }
--- src/Algorithms/TestDriverDevelopment/CalculatorTest.java
+++ src/Algorithms/TestDriverDevelopment/CalculatorTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
  * Created by RENT on 2017-08-10.
  */
 public class CalculatorTest {
-    @Test
-    public void onePlusOneshouldEquelsTwo() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(2, calculator.sum(1, 1));
-}
-        @Test
-        public void twoPlustreeEquelsfive() {
-            Calculator calculator = new Calculator();
-            Assert.assertEquals(5, calculator.sum(3, 2));
+//    @Test
+//    public void onePlusOneshouldEquelsTwo() {
+//        Calculator calculator = new Calculator();
+//        Assert.assertEquals(2, calculator.sum(1, 1));
+//}
+//        @Test
+//        public void twoPlustreeEquelsfive() {
+//            Calculator calculator = new Calculator();
+//            Assert.assertEquals(5, calculator.sum(3, 2));
 
 
 //    public void nothing(){
@@ -27,6 +27,5 @@ public class CalculatorTest {
 
 
         }
-    }
 
 
